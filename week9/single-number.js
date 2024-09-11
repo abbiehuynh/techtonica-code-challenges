38967aef48b02d46cae0be6e72ab17cac6cf3c90
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-11 11:44:24 -0500

    adding solutions to code challenge

--- week9/single-number.js
+++ week9/single-number.js
@@ -11,3 +11,33 @@ Challenge: Can you optimize your implementation to be linear runtime complexity?
 
 
 */
+
+// empty object
+// add each unique number as a key, with a value of 1
+// if the number pops again, increment key value
+// after iterating through the array, return object key that has the value of 1
+
+// solution #1
+function findSingle(numArr) {
+    let numCounts = {};
+
+    for (let i = 0; i < numArr.length; i++) {
+        if(numCounts.hasOwnProperty(numArr[i])) {
+            numCounts[numArr[i]]++;
+        }
+        else {
+            numCounts[numArr[i]] = 1;
+        }
+    }
+    return Object.entries(numCounts).filter(([key, value]) => {return value == 1;})[0][0];
+}
+
+console.log(findSingle([4, 1, 2, 1, 2]));
+
+
+// solution #2
+arr = [4, 1, 2, 1, 2];
+
+let result = arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num));
+
+console.log(result.toString());
