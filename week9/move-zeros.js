6694bd0dd7b826b936bf4b51bc0c6de45b05388e
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-12 12:00:32 -0500

    adding solution to code challenge - thursday

--- week9/move-zeros.js
+++ week9/move-zeros.js
@@ -15,3 +15,50 @@ Output: [0]
 Challenge: Can you write your answer without making a copy of the array. Could you minimize the total number of operations done?
 
 */
+
+// pseudocode
+
+// // create a function takes an array as a parameter 
+// function moveZero(arr) {
+// // create a variable to hold the zero = 0
+// let zero = 0;
+
+// // create a varaible to hold the newArray 
+// let newArr = [];
+// // create a loop to iterate through the array 
+// for (let i = 0; i < arr.length; i++) {
+// // if i = zero 
+//     if (arr[i] === zero) {
+
+//     }
+//         // shift 0
+//         // then unshift zero into array 
+// }
+//     // return array 
+// }
+
+
+// pseudocode
+// intialize a pointer start at 0 to track non-zero elements => nonZeroIndex
+// iterate through array with a loop 
+// swap logic [a, b] = [b, a]
+// increment non zero index 
+// continue looping 
+// return 
+
+function moveZeros(nums){
+    // initialize a pointer start at 0
+    let nonZeroIndex = 0;
+    // Iterate through array
+    for (let i = 0; i < nums.length; i++) {
+        // current element is not zero, swap with nonZeroIndex position
+        if(nums[i] !== 0){
+            // destructuring an array while swapping variables 
+            [nums[i], nums[nonZeroIndex]] = [nums[nonZeroIndex], nums[i]];
+            nonZeroIndex++;
+        }
+    }
+    return nums;
+}
+
+console.log(moveZeros([0, 1, 0, 3, 12]));
